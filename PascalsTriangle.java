3b24a89308e877bb1f7885dd9248e95392d39587
Devansh Goyal <151917394+Devansh334@users.noreply.github.com>
2024-02-17 21:18:07 +0530

    Pascals Triangle leetCode

---
+++ PascalsTriangle.java
@@ -0,0 +1,52 @@
+/*
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+ 
+
+Example 1:
+
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Example 2:
+
+Input: numRows = 1
+Output: [[1]]
+ 
+
+Constraints:
+
+1 <= numRows <= 30
+*/class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        
+        List<List<Integer>> result =new ArrayList<>();
+        if(numRows==0)return result;
+
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+        result.add(row);
+        
+        List<Integer> prev = row;
+
+        for(int i=1;i<numRows;i++){
+            List<Integer> curr =  new ArrayList<>();
+            curr.add(1);
+            for(int j=1;j<i;j++){
+                curr.add(prev.get(j)+prev.get(j-1));
+            }
+            curr.add(1);
+            result.add(curr);
+            prev=curr;
+
+        }
+
+        return result;
+
+    }
+}
+
+
+
