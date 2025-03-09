c525c457b353f3503b49da05a75e85bffe51a435
Devansh Goyal <151917394+Devansh334@users.noreply.github.com>
2025-03-09 17:48:54 +0530

    Koko Eating the Bananas problem leetCode

---
+++ KOKOEatingBananas.java
@@ -0,0 +1,67 @@
+/*
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+
+ 
+
+Example 1:
+
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+Example 2:
+
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+Example 3:
+
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
+ 
+
+Constraints:
+
+1 <= piles.length <= 104
+piles.length <= h <= 109
+1 <= piles[i] <= 109
+
+*/
+
+class Solution {
+    public boolean isValidSpeed(int[] piles, int h, int speed) {
+        int timeRequired = 0;
+
+        for (int pile : piles) {
+            timeRequired += Math.ceil((double) pile / speed); 
+            if (timeRequired > h) {
+                return false; 
+            }
+        }
+        return true;
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = Integer.MIN_VALUE;
+
+
+        for (int pile : piles) {
+            max = Math.max(max, pile);
+        }
+
+        int f = 1, l = max;
+        while (f < l) {  
+            int m = (f + l) / 2;
+
+            if (isValidSpeed(piles, h, m)) {
+                l = m; 
+            } else {
+                f = m + 1; 
+            }
+        }
+        return f; 
+    }
+}
